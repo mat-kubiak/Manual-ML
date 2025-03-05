adf6cd7c2fa6bab0a0c11b6687c8e3bb6fb94523
Mateusz Kubiak <matkubiak888@gmail.com>
2025-03-05 21:22:11 +0100

    refactor: model can have arbitrary number of layers

--- main.py
+++ main.py
@@ -7,49 +7,46 @@ from src.dense_layer import DenseLayer
 def mse(x, y):
     return np.mean((x - y)**2)
 
-class neural_net:
+class LayerStack:
     def __init__(self, units=50):
-        self.hidden_layer = DenseLayer(1, units, 'relu')
-        self.middle_layer = DenseLayer(units, units, 'relu')
-        self.output_layer = DenseLayer(units, 1)
+        hidden_layer = DenseLayer(1, units, 'relu')
+        middle_layer = DenseLayer(units, units, 'relu')
+        output_layer = DenseLayer(units, 1)
+
+        self.layers = [hidden_layer, middle_layer, output_layer]
 
     def apply(self, batch):
         x = batch.reshape(-1, 1)
-        x = self.hidden_layer.apply(x)
-        x = self.middle_layer.apply(x)
-        x = self.output_layer.apply(x)
+        for i in range(len(self.layers)):
+            x = self.layers[i].apply(x)
         return x
 
-    def fit(self, batch_x, batch_y):
-        preds1 = self.apply(batch_x)
-        loss1 = mse(preds1.flatten(), batch_y)
+    def copy(self):
+        return copy.deepcopy(self)
 
-        tries = 3
-        best_hidden = None
-        best_middle = None
-        best_output = None
-        best_loss = loss1
-        for i in range(tries):
-            new_hidden = self.hidden_layer.copy().add_gaussian(stddev=0.035)
-            new_middle = self.middle_layer.copy().add_gaussian(stddev=0.035)
-            new_output = self.output_layer.copy().add_gaussian(stddev=0.035)
+    def add_gaussian(self, stddev):
+        for layer in self.layers:
+            layer.add_gaussian(stddev)
+        return self
 
-            x = batch_x.reshape(-1, 1)
-            x = new_hidden.apply(x)
-            x = new_middle.apply(x)
-            x = new_output.apply(x)
-            loss2 = mse(x.flatten(), batch_y)
-            
-            if loss2 < best_loss:
-                best_hidden = new_hidden
-                best_middle = new_middle
-                best_output = new_output
+def fit(stack, batch_x, batch_y):
+    init_preds = stack.apply(batch_x)
+    init_loss = mse(init_preds.flatten(), batch_y)
 
-        if best_hidden != None:
-            self.hidden_layer = best_hidden
-            self.middle_layer = best_middle
-            self.output_layer = best_output
-            print(f'loss: {best_loss}')
+    best_stack = stack
+    best_loss = init_loss
+
+    tries = 3
+    for i in range(tries):
+        new_stack = stack.copy().add_gaussian(stddev=0.035)
+        new_preds = new_stack.apply(batch_x)
+        new_loss = mse(new_preds.flatten(), batch_y)
+
+        if new_loss < best_loss:
+            best_stack = new_stack
+
+    print(f'loss: {best_loss}')
+    return best_stack
 
 def main():
 
@@ -57,7 +54,7 @@ def main():
     x = np.linspace(0, 1, 100, dtype=np.float32)
     y = np.sin(x * 10)
 
-    model = neural_net(units=15)
+    model = LayerStack(units=15)
 
     batch_size = 10
     num_batches = len(x) // batch_size
@@ -65,7 +62,7 @@ def main():
     # fitting
     epochs = 10000
     for e in range(epochs):
-        model.fit(x, y)
+        model = fit(model, x, y)
 
     # evaluating
     preds = np.empty((0,), dtype=np.float32)
