ad4bed9eefbcd3e796a179f181831fdf7f7f0394
Mateusz Kubiak <matkubiak888@gmail.com>
2025-03-06 23:07:39 +0100

    feat: implement buggy backpropagation

--- src/model.py
+++ src/model.py
@@ -22,6 +22,90 @@ def naive_optimizer(stack, loss, batch_x, batch_y):
 
     return best_stack, best_loss
 
+def great_optimizer(stack, loss, batch_x, batch_y):
+    bias_gradients, weight_gradients = real_backprop(stack, batch_x, batch_y)
+
+    lr_rate = 0.0001
+
+    for i, layer in enumerate(stack.layers):
+        layer.biases = layer.biases - lr_rate * bias_gradients[i]
+        layer.weights = layer.weights - lr_rate * weight_gradients[i]
+    
+    pred = stack.apply(batch_x)
+    loss_val = loss(batch_y, pred)
+
+    return stack, loss_val
+
+def loss_derivative(y_pred, y_true):
+    return 2.0*(y_pred-y_true)
+
+def activation_derivative(x):
+    return np.where(x > 0, 1.0, 0.0).astype(np.float32)
+
+def real_backprop(stack: LayerStack, x, y_true):
+    activations = stack.get_activations(x)
+    y_pred = activations[len(activations)-1].flatten()
+
+    bias_gradients = []
+    weight_gradients = []
+
+    # initialize delta
+    delta = loss_derivative(y_pred, y_true).reshape(-1, 1)
+
+    for i in range(len(stack.layers)-1, -1, -1):
+        prev_act = activations[i-1] if i != 0 else x.reshape(-1,1)
+        
+        current_layer = stack.layers[i]
+        prev_layer = stack.layers[i-1]
+        weights = current_layer.weights
+
+        if not current_layer.is_activation_linear:
+            act_deriv = activation_derivative(prev_act)
+            b_grad = np.sum(delta * act_deriv, axis=0, keepdims=False)
+        else:
+            b_grad = np.sum(delta, axis=0, keepdims=False)
+
+        w_grad = np.matmul(prev_act.T, delta)
+
+        bias_gradients.append(b_grad)
+        weight_gradients.append(w_grad)
+
+        if i != 0:
+            delta = np.matmul(delta, weights.T)
+
+    return list(reversed(bias_gradients)), list(reversed(weight_gradients))
+
+    # last layer
+    b_grad = np.sum(delta, axis=0, keepdims=False)
+    w_grad = np.matmul(activations[lnum-2].T, delta)
+
+    bias_gradients.append(b_grad)
+    weight_gradients.append(w_grad)
+
+    delta = np.matmul(delta, stack.layers[-1].weights.T)
+    
+    # second-to-last layer
+    act_deriv = activation_derivative(activations[lnum-2])
+    
+    b_grad = np.sum(delta * act_deriv, axis=0, keepdims=False)
+    w_grad = np.matmul(activations[lnum-3].T, delta)
+
+    bias_gradients.append(b_grad)
+    weight_gradients.append(w_grad)
+
+    delta = np.matmul(delta, stack.layers[-2].weights.T)
+    
+    # first layer
+    act_deriv = activation_derivative(activations[lnum-3])
+    
+    b_grad = np.sum(delta * act_deriv, axis=0, keepdims=False)
+    w_grad = np.matmul(x.T, delta)
+
+    bias_gradients.append(b_grad)
+    weight_gradients.append(w_grad)
+
+    return list(reversed(bias_gradients)), list(reversed(weight_gradients))
+
 class Model:
     def __init__(self, layers, loss):
         self.stack = LayerStack(layers)
