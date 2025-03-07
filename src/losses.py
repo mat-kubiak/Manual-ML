6d24f7cb8601d5efdf9ddf6e52e860ac7b350e08
Mateusz Kubiak <matkubiak888@gmail.com>
2025-03-07 02:53:49 +0100

    refactor: OOP losses

--- src/losses.py
+++ src/losses.py
@@ -1,16 +1,33 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-def mse(x, y):
-    return np.mean((x - y)**2)
+class Loss(ABC):
+    @abstractmethod
+    def apply(self, x, y):
+        pass
 
-def mae(x, y):
-    return np.mean(np.absolute(x - y))
+    def __call__(self, x, y):
+        return self.apply(x, y)
+
+    def get_name(self):
+        return type(self).__name__.lower()
+
+class MSE(Loss):
+    def apply(self, x, y):
+        return np.mean((x - y)**2)
+
+class MAE(Loss):
+    def apply(self, x, y):
+        return np.mean(np.absolute(x - y))
+
+_losses = {
+    cls().get_name(): cls
+    for cls in Loss.__subclasses__()
+}
 
 def get_loss(name):
-    fns = {
-        'mse': mse,
-        'mae': mae,
-    }
-    if name not in fns.keys():
-        raise Exception(f'Loss function `{name}` not found! Available losses: [{', '.join(fns.keys())}]')
-    return fns[name]
+    name = name.strip().lower()
+    if name not in _losses.keys():
+        available = ', '.join(_losses.keys())
+        raise ValueError(f'Loss function `{name}` not found! Available losses: [{available}]')
+    return _losses[name]()
