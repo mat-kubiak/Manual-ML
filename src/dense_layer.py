5a8751f72e35c6535699535d80dd62af1e4c5512
Mateusz Kubiak <matkubiak888@gmail.com>
2025-03-08 03:13:24 +0100

    refactor: remove is_activation_linear param of DenseLayer

--- src/dense_layer.py
+++ src/dense_layer.py
@@ -1,13 +1,12 @@
 import copy
 import numpy as np
-from src.activations import ensure_activation, is_activation_linear
+from src.activations import ensure_activation
 
 class DenseLayer:
     def __init__(self, input_shape, units, activation=''):
         self.input_shape = input_shape
         self.output_shape = units
         self.activation = ensure_activation(activation)
-        self.is_activation_linear = is_activation_linear(self.activation)
 
         stddev = 0.2
         self.biases = np.random.normal(0.0, stddev, [units]).astype(np.float32)
