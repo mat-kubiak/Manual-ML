b18fce9c2ae94c952fbad0d077c8836e3862a86a
Mateusz Kubiak <matkubiak888@gmail.com>
2025-03-26 17:15:11 +0100

    refactor: introduce Layer base class and update optimizers for compatibility

--- src/dense_layer.py
+++ src/dense_layer.py
@@ -1,24 +1,61 @@
 import copy
+from abc import ABC, abstractmethod
 import numpy as np
 from src.activations import ensure_activation
 from src.initializers import ensure_initializer
 
-class DenseLayer:
-    def __init__(self, input_shape, units, activation='', initializer=''):
+class Layer(ABC):
+    def __init__(self, input_shape, output_shape):
         self.input_shape = input_shape
-        self.output_shape = units
+        self.output_shape = output_shape
+        self.params = {}
+
+    @abstractmethod
+    def apply_linear(self, tensor):
+        pass
+
+    @abstractmethod
+    def apply(self, tensor):
+        pass
+
+    @abstractmethod
+    def backward(self, z_input, prev_act, delta):
+        pass
+
+class DenseLayer(Layer):
+    def __init__(self, input_shape, units, activation='', initializer=''):
+        super().__init__(input_shape, units)
         self.activation = ensure_activation(activation)
         self.initializer = ensure_initializer(initializer)
 
-        self.biases = np.zeros((self.output_shape), dtype=np.float32)
-        self.weights = self.initializer((self.input_shape, self.output_shape))
+        self.params = {
+            'biases': np.zeros((self.output_shape), dtype=np.float32),
+            'weights': self.initializer((self.input_shape, self.output_shape))
+        }
 
     def apply(self, tensor):
-        tensor = np.matmul(tensor, self.weights, dtype=np.float32) + self.biases
+        tensor = np.matmul(tensor, self.params['weights'], dtype=np.float32) + self.params['biases']
         return self.activation(tensor)
 
     def apply_linear(self, tensor):
-        return np.matmul(tensor, self.weights, dtype=np.float32) + self.biases
+        return np.matmul(tensor, self.params['weights'], dtype=np.float32) + self.params['biases']
+
+    def backward(self, z_input, prev_act, delta):
+        # propagate error to before layer activation
+        act_deriv = self.activation.apply_derivative(z_input)
+        delta = delta * act_deriv
+
+        b_grad = np.sum(delta, axis=0, keepdims=True)
+        w_grad = np.matmul(prev_act.T, delta)
+
+        gradient = {}
+        gradient['biases'] = b_grad.astype(np.float32)
+        gradient['weights'] = w_grad.astype(np.float32)
+
+        # propagate error to before current layer
+        delta = np.matmul(delta, self.params['weights'].T)
+
+        return gradient, delta
 
     def copy(self):
         return copy.deepcopy(self)
--- src/optimizers.py
+++ src/optimizers.py
@@ -36,33 +36,20 @@ class RandomOptimizer(Optimizer):
 def _backprop_gradient(stack: LayerStack, loss: Loss, x, y_true):
     activations, z_inputs, y_pred = stack.forward_trace(x)
 
-    bias_gradients = []
-    weight_gradients = []
+    gradients = []
 
     # propagate error to before loss
     delta = loss.apply_derivative(y_pred, y_true)
     if len(delta.shape) == 1:
         delta = delta.reshape(-1, 1)
 
+    # propagate through layers
     for i in range(len(stack.layers)-1, -1, -1):
-        # propagate error to before layer activation
-        act_deriv = stack.layers[i].activation.apply_derivative(z_inputs[i])
-        delta = delta * act_deriv
-
         prev_act = activations[i-1] if i > 0 else x
-        
-        b_grad = np.sum(delta, axis=0, keepdims=True)
-        w_grad = np.matmul(prev_act.T, delta)
+        gradient, delta = stack.layers[i].backward(z_inputs[i], prev_act, delta)
+        gradients.append(gradient)
 
-        bias_gradients.append(b_grad)
-        weight_gradients.append(w_grad)
-
-        # propagate error to before current layer
-        # (omit first layer because it won't be used)
-        if i != 0:
-            delta = np.matmul(delta, stack.layers[i].weights.T)
-
-    return [x.astype(np.float32) for x in reversed(bias_gradients)], [x.astype(np.float32) for x in reversed(weight_gradients)]
+    return [x for x in reversed(gradients)]
 
 class SGD(Optimizer):
     def __init__(self, lr_rate=0.01, momentum=0.9):
@@ -70,28 +57,24 @@ class SGD(Optimizer):
         self.momentum = momentum
         self.velocity = None
 
-    def _update_velocity(self, b_grad, w_grad):
-            if self.velocity == None:
-                self.velocity = {
-                    'biases': b_grad,
-                    'weights': w_grad
-                }
-                return
+    def _update_velocity(self, grad):
+        if self.velocity == None:
+            self.velocity = grad
+            return
 
-            b_vel = self.velocity['biases']
-            w_vel = self.velocity['weights']
-
-            for i in range(len(b_vel)):
-                b_vel[i] = b_vel[i] * self.momentum + b_grad[i] * (1-self.momentum)
-                w_vel[i] = w_vel[i] * self.momentum + w_grad[i] * (1-self.momentum)
+        for i in range(len(grad)):
+            for k in grad[i].keys():
+                self.velocity[i][k] = self.velocity[i][k] * self.momentum + grad[i][k] * (1 - self.momentum)
 
     def apply(self, stack, loss, batch_x, batch_y):
-        bias_gradients, weight_gradients = _backprop_gradient(stack, loss, batch_x, batch_y)
-        self._update_velocity(bias_gradients, weight_gradients)
+        gradients = _backprop_gradient(stack, loss, batch_x, batch_y)
+        self._update_velocity(gradients)
 
         for i, layer in enumerate(stack.layers):
-            layer.biases = layer.biases - self.lr_rate * self.velocity['biases'][i]
-            layer.weights = layer.weights - self.lr_rate * self.velocity['weights'][i]
+            layer.params = {
+                k: layer.params[k] - self.lr_rate * self.velocity[i][k]
+                for k in layer.params.keys()
+            }
 
         pred = stack.apply(batch_x)
         loss_val = loss(pred, batch_y)
@@ -105,51 +88,29 @@ class Adam(Optimizer):
         self.velocity = None
         self.second_velocity = None
 
-    def _update_velocity(self, b_grad, w_grad):
+    def _update_velocity(self, grad):
             if self.velocity == None:
-                self.velocity = {
-                    'biases': b_grad,
-                    'weights': w_grad
-                }
-                self.second_velocity = {
-                    'biases': [np.square(layer) for layer in b_grad],
-                    'weights': [np.square(layer) for layer in w_grad]
-                }
+                self.velocity = grad
+                self.second_velocity = [{key: np.square(layer[key]) for key in layer.keys()} for layer in grad]
                 return
 
-            b_vel = self.velocity['biases']
-            w_vel = self.velocity['weights']
-
-            b_vel_2 = self.second_velocity['biases']
-            w_vel_2 = self.second_velocity['weights']
-
-            for i in range(len(b_vel)):
-                b_vel[i] = b_vel[i] * self.momentum + b_grad[i] * (1-self.momentum)
-                w_vel[i] = w_vel[i] * self.momentum + w_grad[i] * (1-self.momentum)
-
-                b_vel_2[i] = b_vel_2[i] * self.momentum + np.square(b_grad[i]) * (1-self.momentum)
-                w_vel_2[i] = w_vel_2[i] * self.momentum + np.square(w_grad[i]) * (1-self.momentum)
+            for i in range(len(grad)):
+                for k in grad[i].keys():
+                    self.velocity[i][k] = self.velocity[i][k] * self.momentum + grad[i][k] * (1 - self.momentum)
+                    self.second_velocity[i][k] = self.second_velocity[i][k] * self.momentum + np.square(grad[i][k]) * (1 - self.momentum)
 
     def apply(self, stack, loss, batch_x, batch_y):
-        bias_gradients, weight_gradients = _backprop_gradient(stack, loss, batch_x, batch_y)
-        self._update_velocity(bias_gradients, weight_gradients)
+        gradients = _backprop_gradient(stack, loss, batch_x, batch_y)
+        self._update_velocity(gradients)
 
         epsilon = np.finfo(np.float32).eps
 
         for i, layer in enumerate(stack.layers):
 
-            unbiased_momentum_1 = {
-                'biases': self.velocity['biases'][i] / (1 - self.momentum),
-                'weights': self.velocity['weights'][i] / (1 - self.momentum)
-            }
-
-            unbiased_momentum_2 = {
-                'biases': self.second_velocity['biases'][i] / (1 - self.momentum),
-                'weights': self.second_velocity['weights'][i] / (1 - self.momentum)
-            }
-
-            layer.biases = layer.biases - self.lr_rate * (unbiased_momentum_1['biases']) / (np.sqrt(unbiased_momentum_2['biases']) + epsilon)
-            layer.weights = layer.weights - self.lr_rate * (unbiased_momentum_1['weights']) / (np.sqrt(unbiased_momentum_2['weights']) + epsilon)
+            for k in layer.params.keys():
+                unbiased_momentum_1 = self.velocity[i][k] / (1 - self.momentum)
+                unbiased_momentum_2 = self.second_velocity[i][k] / (1 - self.momentum)
+                layer.params[k] = layer.params[k] - self.lr_rate * (unbiased_momentum_1) / (np.sqrt(unbiased_momentum_2) + epsilon)
 
         pred = stack.apply(batch_x)
         loss_val = loss(pred, batch_y)
