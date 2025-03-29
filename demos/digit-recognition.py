b2a2b68a097fdfcdead971d5e792d8590d6b2084
Mateusz Kubiak <matkubiak888@gmail.com>
2025-03-29 01:09:43 +0100

    feat: setup a basic fully-connected network for MNIST demo

--- demos/digit-recognition.py
+++ demos/digit-recognition.py
@@ -1,6 +1,13 @@
 import os, hashlib, urllib.request
 from pathlib import Path
+
 import numpy as np
+import matplotlib.pyplot as plt
+
+from src.layers import DenseLayer
+from src.model import Model
+from src import optimizers
+from src.progress_bar import DownloadProgressBar
 
 class DLProgbar:
     def __init__(self):
@@ -63,6 +70,54 @@ def main():
     # one-hot encode
     y_train = np.eye(10)[y_train]
     y_test = np.eye(10)[y_test]
+
+    model = Model(
+        loss='categorical_crossentropy',
+        metrics=['accuracy'],
+        optimizer=optimizers.Adam(lr_rate=1e-4),
+        layers=[
+            DenseLayer(28 * 28, 512, 'leaky_relu'),
+            DenseLayer(512, 256, 'leaky_relu'),
+            DenseLayer(256, 128, 'leaky_relu'),
+            DenseLayer(128, 64, 'leaky_relu'),
+            DenseLayer(64, 10, 'softmax')
+        ]
+    )
+
+    fig, ax = plt.subplots()
+
+    # init graphs
+    loss_line, = ax.plot([], [], color='red', linewidth=2.0)
+    ax.set_title("Loss History")
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Loss")
+    ax.set_yscale('log')
+    ax.grid()
+
+    plt.tight_layout()
+    plt.show(block=False)
+
+    loss_history = []
+
+    def update_plot_callback(epoch, metrics):
+        preds = model.apply(x_train)
+        loss_history.append(np.mean(model.loss(preds, y_train)))
+
+        loss_line.set_xdata(range(len(loss_history)))
+        loss_line.set_ydata(loss_history)
+
+        ax.set_xlim(0, len(loss_history))
+        ax.relim()
+        ax.autoscale_view()
+
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+    model.fit(x_train, y_train,
+        batch_size=32,
+        epochs=1,
+        epoch_callback=update_plot_callback
+    )
 
 if __name__ == '__main__':
     main()
